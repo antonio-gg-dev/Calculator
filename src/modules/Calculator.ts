bee67f86acc28643fdc8dcd9a705008885965508
katarn <katarn@katarn.es>
2023-04-07 00:45:26 +0200

    addNumber ts restrictions

--- src/modules/Calculator.ts
+++ src/modules/Calculator.ts
@@ -14,7 +14,7 @@ export class Calculator {
       .join(' ')
   }
 
-  public addNumber (number: number): Calculator {
+  public addNumber (number: 0|1|2|3|4|5|6|7|8|9): Calculator {
     this.firstNumber = Number(`${this.firstNumber}${number}`)
 
     return this

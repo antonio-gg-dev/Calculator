dbb5b454fe3c9c3097b5c7a311e72325d1755173
katarn <katarn@katarn.es>
2023-04-06 14:09:02 +0200

    Add more tests

--- tests/modules/Calculator.spec.ts
+++ tests/modules/Calculator.spec.ts
@@ -7,47 +7,93 @@ describe('@/modules/Calculator', () => {
     calculator = new Calculator()
   })
 
-  it('should print 0 at start', () => {
+  it('should print "0" at start', () => {
     expect(calculator.print()).toBe('0')
   })
 
-  it('should print 1 when number 1 added', () => {
-    calculator.addNumber(1)
+  it.each`
+    given | expected
+    ${0}  | ${'0'}
+    ${1}  | ${'1'}
+    ${2}  | ${'2'}
+    ${3}  | ${'3'}
+    ${8}  | ${'8'}
+  `('should print "$expected" when number $given added', ({ given, expected }) => {
+    calculator.addNumber(given)
 
-    expect(calculator.print()).toBe('1')
+    expect(calculator.print()).toBe(expected)
   })
 
-  it('should print 2 when number 2 added', () => {
-    calculator.addNumber(2)
+  it.each`
+    givenFirst | givenSecond | expected
+    ${0}       | ${6}        | ${'6'}
+    ${2}       | ${6}        | ${'26'}
+    ${6}       | ${5}        | ${'65'}
+    ${7}       | ${0}        | ${'70'}
+    ${8}       | ${6}        | ${'86'}
+  `(
+    'should print "$expected" when number $givenFirst added then $givenSecond', ({ givenFirst, givenSecond, expected }) => {
+      calculator.addNumber(givenFirst)
+        .addNumber(givenSecond)
 
-    expect(calculator.print()).toBe('2')
+      expect(calculator.print()).toBe(expected)
+    })
+
+  it.each`
+    givenFirst | givenSecond | givenThird | expected
+    ${0}       | ${0}        | ${9}       | ${'9'}
+    ${0}       | ${4}        | ${2}       | ${'42'}
+    ${3}       | ${0}        | ${0}       | ${'300'}
+    ${7}       | ${9}        | ${9}       | ${'799'}
+    ${9}       | ${8}        | ${9}       | ${'989'}
+  `('should print "$expected" when number $givenFirst added then $givenSecond and $givenThird', ({ givenFirst, givenSecond, givenThird, expected }) => {
+    calculator.addNumber(givenFirst)
+      .addNumber(givenSecond)
+      .addNumber(givenThird)
+
+    expect(calculator.print()).toBe(expected)
   })
 
-  it('should print 10 when number 1 added then 0', () => {
-    calculator.addNumber(1)
-      .addNumber(0)
-
-    expect(calculator.print()).toBe('10')
-  })
-
-  it('should print 0 when number 1 added then backspace', () => {
-    calculator.addNumber(1)
+  it.each`
+    given
+    ${0}
+    ${2}
+    ${5}
+    ${8}
+    ${6}
+  `('should print "0" when number $given added then backspace', ({ given }) => {
+    calculator.addNumber(given)
       .doBackspace()
 
     expect(calculator.print()).toBe('0')
   })
 
-  it('should print 1 when number 10 added then backspace', () => {
-    calculator.addNumber(1)
-      .addNumber(0)
+  it.each`
+    givenFirst | givenSecond | expected
+    ${0}       | ${7}        | ${'0'}
+    ${3}       | ${9}        | ${'3'}
+    ${4}       | ${4}        | ${'4'}
+    ${6}       | ${3}        | ${'6'}
+    ${8}       | ${0}        | ${'8'}
+  `('should print "$expected" when number $givenFirst$givenSecond added then backspace', ({ givenFirst, givenSecond, expected }) => {
+    calculator.addNumber(givenFirst)
+      .addNumber(givenSecond)
       .doBackspace()
 
-    expect(calculator.print()).toBe('1')
+    expect(calculator.print()).toBe(expected)
   })
 
-  it('should print 1 when number 10 added then clear', () => {
-    calculator.addNumber(1)
-      .addNumber(0)
+  it.each`
+    givenFirst | givenSecond | givenThird
+    ${0}       | ${0}        | ${0}
+    ${0}       | ${0}        | ${3}
+    ${0}       | ${7}        | ${4}
+    ${4}       | ${1}        | ${2}
+    ${2}       | ${9}        | ${8}
+  `('should print "0" when number $givenFirst$givenSecond$givenThird added then clear', ({ givenFirst, givenSecond, givenThird }) => {
+    calculator.addNumber(givenFirst)
+      .addNumber(givenSecond)
+      .addNumber(givenThird)
       .doClear()
 
     expect(calculator.print()).toBe('0')

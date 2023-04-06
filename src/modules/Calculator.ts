7d415f71530be0b5f7873e75ba41c609576cf575
katarn <katarn@katarn.es>
2023-04-06 02:09:19 +0200

    Calculator should print 1 when number 1 added

--- src/modules/Calculator.ts
+++ src/modules/Calculator.ts
@@ -1,5 +1,16 @@
 export class Calculator {
+  private firstNumber = false
   public print (): string {
+    if (this.firstNumber) {
+      return '1'
+    }
+
     return '0'
+  }
+
+  public addNumber (number: number): Calculator {
+    this.firstNumber = true
+
+    return this
   }
 }
--- tests/modules/Calculator.spec.ts
+++ tests/modules/Calculator.spec.ts
@@ -1,9 +1,19 @@
 import { Calculator } from '@/modules/Calculator'
 
 describe('', () => {
-  it('should print 0 at start', () => {
-    const calculator = new Calculator()
+  let calculator: Calculator
 
+  beforeEach(() => {
+    calculator = new Calculator()
+  })
+
+  it('should print 0 at start', () => {
     expect(calculator.print()).toBe('0')
+  })
+
+  it('should print 1 when number 1 added', () => {
+    calculator.addNumber(1)
+
+    expect(calculator.print()).toBe('1')
   })
 })

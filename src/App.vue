eae95393f8d3a0b3b952dc2c58d520b0ba148441
katarn <katarn@katarn.es>
2023-05-07 21:26:05 +0200

    Theme switcher in the app and detect user prefs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app__container">
     <CalculatorComponent :calculator="calculator" />
+    <input
+      v-for="theme in Themes"
+      @click="themeSwitcher.setTheme(theme)"
+      :key="theme"
+      type="button"
+      :value="theme"
+    >
   </div>
 </template>
 
@@ -8,13 +15,23 @@
 import { defineComponent } from 'vue'
 import CalculatorComponent from '@/components/Calculator.vue'
 import { Calculator as CalculatorService } from '@/modules/Calculator'
+import { Theme, ThemeSwitcher } from '@/modules/ThemeSwitcher'
+import Button from '@/components/Button.vue'
 
 export default defineComponent({
+  computed: {
+    Themes () {
+      return Theme
+    }
+  },
   components: { CalculatorComponent },
   data: () => ({
-    calculator: new CalculatorService()
-  })
-
+    calculator: new CalculatorService(),
+    themeSwitcher: new ThemeSwitcher()
+  }),
+  created () {
+    this.themeSwitcher.setUserTheme()
+  }
 })
 </script>
 
--- src/modules/ThemeSwitcher.ts
+++ src/modules/ThemeSwitcher.ts
@@ -5,4 +5,29 @@ export enum Theme {
 }
 
 export class ThemeSwitcher {
+  async setTheme (theme: Theme) {
+    const body = document.querySelector('body')
+
+    if (!body) {
+      return
+    }
+
+    body.className += ' stop-animations'
+    // eslint-disable-next-line no-unused-expressions
+    body.offsetHeight // NOTE: Workaround to force CSS changes
+    body.className = `${theme} stop-animations`
+    // eslint-disable-next-line no-unused-expressions
+    body.offsetHeight // NOTE: Workaround to force CSS changes
+    body.className = theme
+  }
+
+  async setUserTheme () {
+    let theme = Theme.light
+
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      theme = Theme.dark
+    }
+
+    await this.setTheme(theme)
+  }
 }
